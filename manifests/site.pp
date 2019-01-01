cff94457c77f65846297f151f2b673499f46163b
suresh1566 <43871782+suresh1566@users.noreply.github.com>
2019-01-01 19:34:16 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,10 @@
 node default {
 
 file {'README' :
-path   => '/root/README',
-ensure => file,
+path    => '/root/README',
+ensure  => file,
 content => 'This is read me',
+owner   => root,
 }
 
 }

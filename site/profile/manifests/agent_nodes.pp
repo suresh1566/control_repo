5733a170f4590250c01d1600859041ac99832d97
suresh1566 <43871782+suresh1566@users.noreply.github.com>
2019-01-01 21:03:36 +0530

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,7 +1,7 @@
 class profile::agent_nodes {
 
 include dockeragent
-dockeragent::node {'web.srv.local'}
-dockeragent::node {'db.srv.local'}
+dockeragent::node {'web.srv.local':}
+dockeragent::node {'db.srv.local':}
 
 }
